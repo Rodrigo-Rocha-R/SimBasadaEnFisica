eff99dff9d396bbf92c0d9b71f915c2856f972f1
M0ga <110911133+M0ga@users.noreply.github.com>
2024-02-25 18:37:19 -0600

    Update Gato.cs

--- T_Gato/Assets/Scripts/Gato.cs
+++ T_Gato/Assets/Scripts/Gato.cs
@@ -34,12 +34,13 @@ public class logic
     public static void MakeMatriz()
     {
         matriz = new List<List<int>>();
-        //Generamos los valores de la matriz a un número neutro. 
+        //Generamos los valores de la matriz a un nÃºmero neutro. 
         
         for (int fila = 0; fila <=3; fila++)
         {
             //Se agrega a la matriz una nueva lista. 
             //Ejemplo: [{Genera este espacio},{Genera este espacio},{Genera este espacio}]
+
             matriz.Add(new List<int>());
             
             for(int columna = 0; columna <= 3; columna++)
@@ -48,10 +49,14 @@ public class logic
                 matriz[fila].Add(-1);
             }
         }
+
         //Generamos valores de fila y columna
+
         fila = new List<int>();
         columna = new List<int>();
+
         //Agrega cuantos veces se pueden usar las respectivas filas y columnas
+
         for(int i = 0; i <= 3; i++)
         {
             fila.Add(3);
@@ -88,15 +93,27 @@ public class logic
             
             //Actualiza el valor de la matriz por el jugador (1 o 0)
             matriz[random_fila][random_columa] = turno_jugador;
+
             //Elimina 1 valor possible de la variable fila y columna
+            
             fila[random_fila]--;
             columna[random_columa]--;
+
+            //Hacemos que se compruebe en que turno del juego vamos, 
+            //Para poder empezar a comprobar si hay un ganador 
+            // Se hace desde el 5, ya que es donde empieza a poderse ganar.
+            
             if (turno >=5)
             {
-                //Hacer funcion que verifique quien gana
-                //Si gana salir del for
+                if (CheckWinner() == true)
+                {
+                    Debug.Log("Hubo Ganador");
+                    break;
+                }
             }
+
             //Cambia al jugador, 1 y 1
+
             if(turno_jugador == 1)
             {
                 turno_jugador--;
@@ -107,7 +124,7 @@ public class logic
             }
             
         }
-        
+        /* Quitar comentado, para probar lo que saca
         Debug.Log(matriz[0][0]);
         Debug.Log(matriz[0][1]);
         Debug.Log(matriz[0][2]);
@@ -117,7 +134,36 @@ public class logic
         Debug.Log(matriz[2][0]);
         Debug.Log(matriz[2][1]);
         Debug.Log(matriz[2][2]);
+        */
         
     }
-       
+
+    public static bool CheckWinner()
+    {
+        //Revisar diagonal
+
+        if (matriz[0][0] == matriz[1][1] && matriz[0][0] == matriz[2][2] | matriz[0][2] == matriz[1][1] && matriz[0][2] == matriz[2][0])
+        {
+            return true;
+        }
+
+        //Revisar si gano vertical
+
+        if (matriz[0][0] == matriz[0][1] && matriz[0][0] == matriz[0][2] | matriz[1][0] == matriz[1][1] && matriz[1][0] == matriz[1][2] | matriz[2][0] == matriz[2][1] && matriz[2][0] == matriz[2][2])
+        {
+            return true;
+        }
+
+        //Revisar si gano horizontal
+
+        if (matriz[0][0] == matriz[1][0] && matriz[0][0]  == matriz[2][0] | matriz[0][0] == matriz[1][0] && matriz[0][0] == matriz[2][0] | matriz[0][1] == matriz[1][1] && matriz[0][1] == matriz[2][1])
+        {
+            return true;
+        }
+
+        //No hubo Ganador
+
+        return false;
+
+    }
 }
