b74b106a8eac81a86f37db9d6614b902c876d37f
Rodrigo_R <110933152+Rodrigo-Rocha-R@users.noreply.github.com>
2024-02-26 14:22:16 -0600

    v2
    Falta agregar prefabs

---
+++ T_GatoV1/Assets/Scripts/JugarGato.cs
@@ -0,0 +1,190 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Integrantes del Equipo:
+//   Santiago Moreno Lacalle Quintero, A01663197 
+//   Rodrigo Rocha Rosales, A01784108
+
+
+public class Tablero : MonoBehaviour
+{
+
+    public GameObject player1;
+    public GameObject player2;
+
+    GameObject linea;
+    GameObject Piso;
+    GameObject Fg_jugador;
+
+    public Material m_piso;
+    public Material m_tablero;
+
+    // Funcion para crear el piso del Gato.
+   
+    void CreatePiso()
+    {
+        //Crear el piso del gato con dimensiones especificas. 
+
+        Piso = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Piso.name = "Base";
+        Piso.transform.position = new Vector3(0, 0, 0);
+        Piso.transform.localScale = new Vector3(20, 0.1f, 20);
+        //Equipar al objeto con MeshRenderer
+        MeshRenderer mr = Piso.GetComponent<MeshRenderer>();
+        mr.material.color = new Color32(29, 140, 255,255);
+        MeshRenderer mr2 = Piso.gameObject.GetComponent<MeshRenderer>();
+        mr2.material = m_tablero;
+
+        Rigidbody rigidbody = mr.gameObject.AddComponent<Rigidbody>();
+        rigidbody.mass = 20;
+        rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+
+
+       
+
+    }
+    //Funcion para crear el ladrillo del gato.
+    void CreateLinea(Vector3 pos, Vector3 scale, Color color)
+    {
+        linea = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        linea.name = "linea";
+        linea.transform.position = pos;
+        linea.transform.localScale = scale;
+
+        MeshRenderer lin = linea.GetComponent<MeshRenderer>();
+        lin.material.color = color;
+
+        Rigidbody rigidbody = lin.gameObject.AddComponent<Rigidbody>();
+        rigidbody.mass = 10;
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionX;
+        rigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
+
+        Renderer ren =lin.gameObject.GetComponent<Renderer>();
+        ren.material = m_piso;
+    }
+
+    void CrearObjeto(List<List<int>> posicion_gato, int fila, int columna)
+    {
+         
+        if (posicion_gato[fila][columna] == -1)
+        {
+            return;
+        }
+        
+        if(posicion_gato[fila][columna] == 0)
+        {
+            //Cargar la textura del jugador 1.
+            Fg_jugador = Instantiate(player1);
+        }
+
+        if(posicion_gato[fila][columna] == 1)
+        {
+            Fg_jugador = Instantiate(player2);
+        }
+
+        // La posicion de donde se va a tirar el objeto
+        Rigidbody rigidbody = Fg_jugador.gameObject.AddComponent<Rigidbody>();
+        rigidbody.mass = 5;
+
+        if (fila == 0)
+        {
+            if(columna == 0)
+            {
+                Fg_jugador.transform.position = new Vector3(-2.4f, 5, 2.4f);
+
+            }
+            
+            if(columna == 1)
+            {
+                Fg_jugador.transform.position = new Vector3(0, 5, 2.4f);
+            }
+
+            if(columna == 2)
+            {
+                Fg_jugador.transform.position = new Vector3(2.4f, 5, 2.4f);
+            }
+        }
+
+        if(fila == 1)
+        {
+            if (columna == 0)
+            {
+                Fg_jugador.transform.position = new Vector3(-2.3f, 5, 0);
+            }
+
+            if (columna == 1)
+            {
+                Fg_jugador.transform.position = new Vector3(0, 5, 0);
+            }
+
+            if (columna == 2)
+            {
+                Fg_jugador.transform.position = new Vector3(2.3f, 5, 0);
+            }
+        }
+
+        if(fila == 2)
+        {
+            if (columna == 0)
+            {
+                Fg_jugador.transform.position = new Vector3(-2.3f, 5, -2);
+            }
+
+            if (columna == 1)
+            {
+                Fg_jugador.transform.position = new Vector3(0, 5, -2);
+            }
+
+            if (columna == 2)
+            {
+                Fg_jugador.transform.position = new Vector3(2.3f, 5, -2);
+            }
+        }
+
+    }
+
+    void Start()
+    {
+        
+        logic.MakeLines();
+
+        List<bool> lugares = logic.lines;
+        CreatePiso();
+
+        for (int xy = 0; xy < 13; xy++)
+        {
+            float dxy = xy - 5 + 0.5f;
+            Vector3 scaleH = new Vector3(0.5f, 0.5f, 12);
+            Vector3 scaleV = new Vector3(12, 0.5f, 0.5f);
+
+            if (lugares[xy])
+            {
+                //  línea horizontal
+                CreateLinea(new Vector3((dxy), 0.25f, 0), scaleH, Color.black);
+            }
+            if (lugares[xy])
+            {
+                //  línea vertical
+                CreateLinea(new Vector3(0.25f, 1, (dxy)), scaleV, Color.black);
+            }
+        }
+
+        logic.MakeMatriz();
+        logic.MakeGame();
+
+        List<List<int>> posicion = logic.matriz;
+        for(int fila = 0; fila <=3; fila++)
+        {
+            for(int columna = 0; columna <= 3; columna++)
+            {
+                CrearObjeto(posicion, fila, columna);
+            }
+        }
+
+
+    }
+}
+
